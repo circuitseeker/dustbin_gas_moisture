0e289e76961f9d08561ee4210e42fa2495f60679
Circuit seeker <145029108+circuitseeker@users.noreply.github.com>
2023-10-03 20:16:09 +0530

    Update smartdb_withgas.ino

--- smartdb_withgas.ino
+++ smartdb_withgas.ino
@@ -1,22 +1,28 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 const int trigPin = 12;  // Ultrasonic sensor trigger pin
 const int echoPin = 13; // Ultrasonic sensor echo pin
 const int moisturePin = A0; // Analog pin for soil moisture sensor
 const int gasPin = A1; // Analog pin for gas sensor
+const int irSensorPin = 3; // Digital pin for IR sensor
 
 const int buzzerPin = 2;  // Digital pin for the buzzer
 const int servoPin = 9;   // Digital pin for the servo motor
+
 Servo servo;
 
-const float Vcc = 5.0;       // Voltage supplied to the sensors
+const float Vcc = 5.0;  // Voltage supplied to the sensors
 
+SoftwareSerial bluetooth(4, 5); // RX, TX pins for Bluetooth communication
 
 void setup() {
-  Serial.begin(9600);        // Initialize serial communication
+  Serial.begin(9600);  // Initialize serial communication
+  bluetooth.begin(9600); // Initialize Bluetooth communication
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(irSensorPin, INPUT);
   servo.attach(servoPin);
 }
 
@@ -40,6 +46,9 @@ void loop() {
   float voltage = gasValue * (Vcc / 1024.0);
   float ppm = (voltage - 0.2) * 50.0;
 
+  // IR Sensor (Bin Full)
+  int irSensorValue = digitalRead(irSensorPin);
+
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
@@ -51,25 +60,29 @@ void loop() {
   Serial.print("Gas Concentration (PPM): ");
   Serial.println(ppm);
 
+  // Check if IR sensor detects an object (bin is full)
+  if (irSensorValue == HIGH) {
+    Serial.println("IR sensor detected an object. Activating buzzer.");
+    digitalWrite(buzzerPin, LOW);
+    bluetooth.println("Bin is full. Please empty it.");
+  } else {
+    digitalWrite(buzzerPin, HIGH);
+  }
+
   // Actions based on sensor readings
-  if (distance < 20) {
+  if (distance < 10) {
     if (moisturePercentage > 50) {
-      servo.write(90);  // Rotate the servo to 90 degrees
+      servo.write(90);
     } else {
-      servo.write(180); // Rotate the servo to 180 degrees
+      servo.write(180);
     }
 
     if (ppm > 60) {
-      digitalWrite(buzzerPin, HIGH); // Activate the buzzer
-    } else {
-      digitalWrite(buzzerPin, LOW); // Deactivate the buzzer
+      bluetooth.println("Gas concentration is high. Ventilate the area.");
     }
-
-    delay(1000); // Buzzer on/off time (adjust as needed)
   } else {
-    servo.write(0); // Reset servo to initial position
-    digitalWrite(buzzerPin, LOW); // Deactivate the buzzer
+    servo.write(0);
   }
 
-  delay(1000); // Delay for one second (adjust as needed)
+  delay(1000);
 }
